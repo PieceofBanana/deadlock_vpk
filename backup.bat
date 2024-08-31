b8193cae98891635bf668247cfadc92418206d34
PieceofBanana <3.14eceofshit@gmail.com>
2024-08-31 23:34:51 +0500

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -1,3 +1,3 @@
-decompiler.exe -i "D:\Steam_client\steamapps\common\Project8Staging\game\citadel\pak01_dir.vpk" --output "pak01" --vpk_filepath "scripts" -d --threads 4
+decompiler.exe -i "D:\Steam_client\steamapps\common\Project8Staging\game\citadel\pak01_dir.vpk" --output "pak01" --vpk_filepath "scripts" -d
 robocopy "D:\Steam_client\steamapps\common\Project8Staging\game\citadel\resource\localization\." ".\localization" /e
 robocopy "D:\Steam_client\steamapps\common\Project8Staging\game\citadel" ".\ver" steam.inf
