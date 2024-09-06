13f6566f35d3a0bc76fbab58ed8fecb3c916669f
Piece_of_Banana <3.14eceofshit@gmail.com>
2024-09-06 22:25:38 +0500

    new json files(test)

--- test.bat
+++ test.bat
@@ -1,3 +1,5 @@
 vdata_to_json.py pak01/scripts/abilities.vdata json/abilities/
 vdata_to_json.py pak01/scripts/heroes.vdata json/heroes/
 vdata_to_json.py pak01/scripts/modifiers.vdata json/modifiers/
+vdata_to_json.py pak01/scripts/npc_units.vdata json/npc_units/
+vdata_to_json.py pak01/scripts/misc.vdata json/misc/
