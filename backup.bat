448e81edde4d616e1ed25801dc62d2a5fec19b69
Piece_of_Banana <3.14eceofshit@gmail.com>
2024-09-10 22:43:46 +0500

    ClientVersion=5129

--- backup.bat
+++ backup.bat
@@ -1,4 +1,3 @@
-@echo off
 decompiler.exe -i "D:\Steam_client\steamapps\common\Project8Staging\game\citadel\pak01_dir.vpk" --output "pak01" --vpk_filepath "scripts" -d
 decompiler.exe -i "D:\Steam_client\steamapps\common\Project8Staging\game\citadel\pak01_dir.vpk" --output "pak01" --vpk_filepath "animgraphs" -d
 decompiler.exe -i "D:\Steam_client\steamapps\common\Project8Staging\game\citadel\pak01_dir.vpk" --output "pak01" --vpk_filepath "cfg" -d
@@ -11,8 +10,8 @@ decompiler.exe -i "D:\Steam_client\steamapps\common\Project8Staging\game\citadel
 robocopy "D:\Steam_client\steamapps\common\Project8Staging\game\citadel\resource\localization\." ".\localization" "*english.txt" /e
 robocopy "D:\Steam_client\steamapps\common\Project8Staging\game\citadel\resource\localization\." ".\localization" "*russian.txt" /e
 robocopy "D:\Steam_client\steamapps\common\Project8Staging\game\citadel" ".\ver" steam.inf
-test.bat
-
+call test.bat
 set /p clientver=< .\ver\steam.inf
 git commit -a -m %clientver%
 git push origin
+ping -n 11 127.0.0.1 > nul
